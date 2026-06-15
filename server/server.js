2ed3257ad01384dd2af2a4d1bfa88ebdf0077fa7
aman4122 <www.aman4122@gmail.com>
2026-06-15 10:20:56 +0530

    Base Commit - No real functions till now

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-// ─── Logging ─────────────────────────────────────────────────────────────────
+// ─── Logging 
 if (config.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 } else {
